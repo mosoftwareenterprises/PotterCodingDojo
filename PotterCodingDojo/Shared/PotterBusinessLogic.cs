5f1bf1dc3593faee5ca3571f35c94ed76de67c0a
Mark Oliver <mosoftwareenterprises@gmail.com>
2021-03-31 19:08:13 +0100

    Adding tests for the 5 positions

--- PotterCodingDojo/Shared/PotterBusinessLogic.cs
+++ PotterCodingDojo/Shared/PotterBusinessLogic.cs
@@ -7,7 +7,27 @@ namespace PotterCodingDojo.Shared
     {
         public async Task<decimal> CalculateBestPrice( List<BookInBasket> bookInBaskets )
         {
-            return 8.0M;
+            if (bookInBaskets.Count == 1)
+            {
+                return 8.0M;
+            }
+            else if (bookInBaskets.Count == 2)
+            {
+                return 8.0M*2.0M*0.95M;
+            }
+            else if (bookInBaskets.Count == 3)
+            {
+                return 8.0M * 3.0M * 0.90M;
+            }
+            else if (bookInBaskets.Count == 4)
+            {
+                return 8.0M * 4.0M * 0.80M;
+            }
+            else if (bookInBaskets.Count == 5)
+            {
+                return 8.0M * 5.0M * 0.75M;
+            }
+            return decimal.MaxValue;
         }
     }
 }
--- PotterCodingDojoTests/PotterBusinessLogicTests.cs
+++ PotterCodingDojoTests/PotterBusinessLogicTests.cs
@@ -9,11 +9,15 @@ namespace PotterCodingDojoTests
     public class PotterBusinessLogicTests
     {
         /*
-             One copy of any of the five books costs 8 EUR. If, however, you buy two different books from the series, you get a 5% discount on those two books. If you buy 3 different books, you get a 10% discount. With 4 different books, you get a 20% discount. If you go the whole hog, and buy all 5, you get a huge 25% discount.
+        One copy of any of the five books costs 8 EUR. 
+        If, however, you buy two different books from the series, you get a 5% discount on those two books.
+        If you buy 3 different books, you get a 10% discount.
+        With 4 different books, you get a 20% discount. 
+        If you go the whole hog, and buy all 5, you get a huge 25% discount.
 
-Note that if you buy, say, four books, of which 3 are different titles, you get a 10% discount on the 3 that form part of a set, but the fourth book still costs 8 EUR.*/
+        Note that if you buy, say, four books, of which 3 are different titles, you get a 10% discount on the 3 that form part of a set, but the fourth book still costs 8 EUR.*/
         [TestMethod]
-        public async Task PotterLogic_CalculateBestPrice_OneBook()
+        public async Task CalculateBestPrice_OneBook_FullPrice()
         {
             //Arrange
             var systemUnderTest = CreateSystemUnderTest();
@@ -24,6 +28,84 @@ Note that if you buy, say, four books, of which 3 are different titles, you get 
 
             //Assert
             Assert.AreEqual( 8.0M, bestPriceCalculated, "Incorrect price calculated for a single book" );
+
+        }
+
+        [TestMethod]
+        public async Task CalculateBestPrice_TwoDifferentBooks_5PercentDiscount()
+        {
+            //Arrange
+            var systemUnderTest = CreateSystemUnderTest();
+            List<BookInBasket> testData = new List<BookInBasket> {
+                new BookInBasket { Id = 1, Title = "Philosophers Stone" },
+                new BookInBasket { Id = 2, Title = "Chamber of secrets" }
+            };
+
+            //Act
+            decimal bestPriceCalculated = await systemUnderTest.CalculateBestPrice( testData );
+
+            //Assert
+            Assert.AreEqual( 15.20M, bestPriceCalculated, "Incorrect price calculated for a single book" );
+
+        }
+
+        [TestMethod]
+        public async Task CalculateBestPrice_ThreeDifferentBooks_TenPercentDiscount()
+        {
+            //Arrange
+            var systemUnderTest = CreateSystemUnderTest();
+            List<BookInBasket> testData = new List<BookInBasket> {
+                new BookInBasket { Id = 1, Title = "Philosophers Stone" },
+                new BookInBasket { Id = 2, Title = "Chamber of secrets" },
+                new BookInBasket { Id = 3, Title = "Prisoner of Azkaban" }
+            };
+
+            //Act
+            decimal bestPriceCalculated = await systemUnderTest.CalculateBestPrice( testData );
+
+            //Assert
+            Assert.AreEqual( 21.60M, bestPriceCalculated, "Incorrect price calculated for a single book" );
+
+        }
+
+        [TestMethod]
+        public async Task CalculateBestPrice_FourDifferentBooks_TwentyPercentDiscount()
+        {
+            //Arrange
+            var systemUnderTest = CreateSystemUnderTest();
+            List<BookInBasket> testData = new List<BookInBasket> {
+                new BookInBasket { Id = 1, Title = "Philosophers Stone" },
+                new BookInBasket { Id = 2, Title = "Chamber of secrets" },
+                new BookInBasket { Id = 3, Title = "Prisoner of Azkaban" },
+                new BookInBasket { Id = 4, Title = "Goblet of Fire" }
+            };
+
+            //Act
+            decimal bestPriceCalculated = await systemUnderTest.CalculateBestPrice( testData );
+
+            //Assert
+            Assert.AreEqual( 25.60M, bestPriceCalculated, "Incorrect price calculated for a single book" );
+
+        }
+
+        [TestMethod]
+        public async Task CalculateBestPrice_FiveDifferentBooks_TwentyFivePercentDiscount()
+        {
+            //Arrange
+            var systemUnderTest = CreateSystemUnderTest();
+            List<BookInBasket> testData = new List<BookInBasket> {
+                new BookInBasket { Id = 1, Title = "Philosophers Stone" },
+                new BookInBasket { Id = 2, Title = "Chamber of secrets" },
+                new BookInBasket { Id = 3, Title = "Prisoner of Azkaban" },
+                new BookInBasket { Id = 4, Title = "Goblet of Fire" },
+                new BookInBasket { Id = 5, Title = "Order of the Phoenix" }
+            };
+
+            //Act
+            decimal bestPriceCalculated = await systemUnderTest.CalculateBestPrice( testData );
+
+            //Assert
+            Assert.AreEqual( 30.0M, bestPriceCalculated, "Incorrect price calculated for a single book" );
 
         }
 
