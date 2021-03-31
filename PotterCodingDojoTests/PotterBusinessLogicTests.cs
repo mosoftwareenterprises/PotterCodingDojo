78f89cfdca9acb0f0ea6e7bf94e06b5d0d3e9240
Mark Oliver <mosoftwareenterprises@gmail.com>
2021-03-31 18:13:31 +0100

    Adding test project skeleton

---
+++ PotterCodingDojoTests/PotterBusinessLogicTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace PotterCodingDojoTests
+{
+    [TestClass]
+    public class PotterBusinessLogicTests
+    {
+        [TestMethod]
+        public async Task PotterLogic_Test1()
+        {
+
+        }
+    }
+}
